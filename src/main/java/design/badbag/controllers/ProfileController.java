bb3423df26e2319bed9c88e8ace02adb55400c8a
KerstingJ <kersting.josh@gmail.com>
2017-01-18 18:09:23 -0500

    added ability to edit posts from user profile

--- src/main/java/design/badbag/controllers/ProfileController.java
+++ src/main/java/design/badbag/controllers/ProfileController.java
@@ -38,10 +38,9 @@ public class ProfileController extends AbstractController {
 	@RequestMapping(value = "/profile/{username}", method = RequestMethod.GET)
 	public String userPosts(@PathVariable String username, HttpServletRequest request, Model model) {
 		SiteUser activeUser = getUserFromSession(request.getSession());
-		
+
 		if (activeUser != null) {
 			if (activeUser.getUsername().equals(username)) {
-				System.out.println("this page is editable");
 				model.addAttribute("editable", true);
 			}
 		}
@@ -61,11 +60,12 @@ public class ProfileController extends AbstractController {
 		String postBody = request.getParameter("postBody");
 
 		/*
-		 * author on get request handler for this uri gets user object info from different source
-		 * this might cause confusion in the future although seems accurate under the assumptions that: 
-		 * 		++this will only be called when making edits to page or making a new post 
-		 * 		++only the author of the blog can make edits or make new posts
-		 * 		++the author will be the activeUser
+		 * author on get request handler for this uri gets user object info from
+		 * different source this might cause confusion in the future although
+		 * seems accurate under the assumptions that: ++this will only be called
+		 * when making edits to page or making a new post ++only the author of
+		 * the blog can make edits or make new posts ++the author will be the
+		 * activeUser
 		 */
 		SiteUser author = getUserFromSession(request.getSession());
 
@@ -80,7 +80,29 @@ public class ProfileController extends AbstractController {
 			siteUserDao.save(author);
 		}
 
+		if (request.getParameter("updatePic") != null) {
+			System.out.println("Update Pic!!!");
+		}
+
+				
+		if (request.getParameter("updatePost") != null) {
+			
+			try {
+				
+				Integer upPostUID = Integer.parseInt(request.getParameter("post_ID"));
+				Post updatedPost = postDao.findByUid(upPostUID);
+				updatedPost.setBody(request.getParameter("updatedPost"));
+				postDao.save(updatedPost);
+	
+			} catch (Exception e) {
+				
+				// does it really matter
+				System.out.println("Something went terribly wrong");
+			}
+			
+		}
 		
+
 		if (request.getParameter("newPost") != null) {
 			if (author == null) {
 				System.out.println("You need to be logged in to make a post");
--- src/main/java/design/badbag/models/Post.java
+++ src/main/java/design/badbag/models/Post.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
